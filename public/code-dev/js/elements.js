48b033483f40db45d6a4b1779d2157d795471220
Jason <jason@scantek.com.au>
2021-09-29 22:54:47 +0800

    added up/down arrow support to file list

--- public/code-dev/js/elements.js
+++ public/code-dev/js/elements.js
@@ -233,7 +233,41 @@ class FileItem extends Button {
 		this._refresh.style.visibility = "hidden"
 		this._refresh.hook = "right"
 		this._refresh.setAttribute("size", "tiny")
-		
+		this.on("keydown", e=>{
+		    if(e.code == "ArrowUp") {
+		        e.preventDefault()
+		        if(this.previousElementSibling instanceof FileItem) {
+		           return this.previousElementSibling.focus()
+		        }
+		        if(this.previousElementSibling instanceof Block) {
+		            if(this.previousElementSibling?.lastElementChild instanceof FileItem)
+		           return this.previousElementSibling.lastElementChild.focus()
+		        }
+		        if(this.previousElementSibling?.previousElementSibling instanceof FileItem) {
+		           return this.previousElementSibling.previousElementSibling.focus()
+		        }
+		        if(this.parentElement.previousElementSibling instanceof FileItem) {
+		            return this.parentElement.previousElementSibling.focus()
+		        }
+		    }
+		    if(e.code == "ArrowDown") {
+		        e.preventDefault()
+		        if(this.nextElementSibling instanceof FileItem) {
+		            return this.nextElementSibling.focus()
+		        }
+		        if(this.nextElementSibling instanceof Block) {
+		            if(this.nextElementSibling?.firstElementChild instanceof FileItem)
+		           return this.nextElementSibling.firstElementChild.focus()
+		        }
+		        if(this.nextElementSibling?.nextElementSibling instanceof FileItem) {
+		            return this.nextElementSibling.nextElementSibling.focus()
+		        }
+		        if(this.parentElement.nextElementSibling instanceof FileItem) {
+		            return this.parentElement.nextElementSibling.focus()
+		        }
+		    }
+
+		})
 		return this;
 	}
 	connectedCallback() {
