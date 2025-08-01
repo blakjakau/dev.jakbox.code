e782a8406cfc64a54695a7fe170f864d08cb8b8d
Jason <blakjak.au@gmail.com>
2025-08-01 12:30:52 +0800

    add conduit setup guide

---
+++ code-pwa/js/conduit-setup-guide.mjs
@@ -0,0 +1,14 @@
+export default `
+# Connect to Terminal
+
+To use the integrated terminal, a small, secure helper application called **Conduit** must be installed and running on your local machine. It acts as a bridge to your system's shell.
+
+---
+
+## Getting Started
+
+This is a guided, one-time setup. Follow the steps as they appear below.
+
+<div id="conduit-actions-section" class="conduit-actions"></div>
+`;
+
